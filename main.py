fd93a7e89c295917507adedbf7a6930e686cb4f7
HobbyKode <speedemoniak@yahoo.fr>
2025-02-13 04:26:09 -0400

    Modified main.py to count characters in a text, with a String->Integer dictionary

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the book '{title}'")
+    
+    #Calling the function counting characters
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
 
+    print(f"{num_words} words found in the book '{title}'")
 
 
 def get_num_words(text):
@@ -14,10 +18,24 @@ def get_num_words(text):
     count = len(words)
     return count
 
+
+def get_chars_dict(text):
+    chars = {}
+
+    #checking each character in a lowercase string
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowercase] = 1
+    return  chars
+
+
 def get_book_text(path):
     with open(path) as f:
         book_contents = f.read()
         return book_contents
 
-main()
 
+main()
