0a726482af41ac77e054e402d0ab496ce09d931e
HobbyKode <speedemoniak@yahoo.fr>
2025-02-13 07:07:26 -0400

    main.py now generates a beautiful count report for each letter in a book

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 #Open that file
 def main():
-    title = "Frankenstein"
     #Define path in a variable
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    
     #Calling the function counting characters
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
 
-    print(f"{num_words} words found in the book '{title}'")
+    generate_report(book_path, num_words, chars_dict)
 
 
 def get_num_words(text):
@@ -28,7 +25,7 @@ def get_chars_dict(text):
         if lowered in chars:
             chars[lowered] += 1
         else:
-            chars[lowercase] = 1
+            chars[lowered] = 1
     return  chars
 
 
@@ -37,5 +34,25 @@ def get_book_text(path):
         book_contents = f.read()
         return book_contents
 
+
+def sort_on(dict):
+    return dict["num"]
+
+def generate_report(book_path, num_words, chars_dict):
+    title = "Frankenstein"
+    #Convert the dictionary into a list to simplify the sort
+    freq_list = [{"char": char, "num": count} for char, count in chars_dict.items()]
+    #Non-chronological order sorting
+    freq_list.sort(key =sort_on, reverse = True)
+
+
+    #Displaying my report
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the book '{title}'")
+    #Only displaying letters from the Alphabet
+    for item in freq_list:
+        if item['char'].isalpha():
+            print(f"The '{item['char']}' character was found {item['num']} times")
+    print(f"--- End report ---")
 
 main()
