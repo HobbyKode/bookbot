2fa8ccfef0452cf61319c70c9ff4667e288a881c
HobbyKode <speedemoniak@yahoo.fr>
2025-02-13 03:32:07 -0400

    Modified main.py to count the number of words in a book

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-#Define path in a variable
-path_to_file = "books/frankenstein.txt"
-title = "Frankenstein"
-
 #Open that file
 def main():
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        print(file_contents)
+    title = "Frankenstein"
+    #Define path in a variable
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the book '{title}'")
+
+
+
+def get_num_words(text):
+    words = text.split()
+    count = len(words)
+    return count
+
+def get_book_text(path):
+    with open(path) as f:
+        book_contents = f.read()
+        return book_contents
 
 main()
+
