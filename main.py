55573dfd54263b921b3b1ef9ea077bf401ed7e3c
HobbyKode <speedemoniak@yahoo.fr>
2025-03-16 16:51:34 -0400

    Bookbot's update completedgit add .

--- main.py
+++ main.py
@@ -1,58 +1,27 @@
+from stats import get_num_words, get_chars_dict, generate_report
+import sys
 #Open that file
 def main():
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     #Define path in a variable
-    book_path = "books/frankenstein.txt"
+    #title = "Frankenstein"
+    
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     #Calling the function counting characters
     chars_dict = get_chars_dict(text)
-
+    print(f"{num_words} words found in the document")
+    print(f"{chars_dict}")
     generate_report(book_path, num_words, chars_dict)
-
-
-def get_num_words(text):
-    words = text.split()
-    count = len(words)
-    return count
-
-
-def get_chars_dict(text):
-    chars = {}
-
-    #checking each character in a lowercase string
-    for c in text:
-        lowered = c.lower()
-        if lowered in chars:
-            chars[lowered] += 1
-        else:
-            chars[lowered] = 1
-    return  chars
-
 
 def get_book_text(path):
     with open(path) as f:
         book_contents = f.read()
         return book_contents
 
-
-def sort_on(dict):
-    return dict["num"]
-
-def generate_report(book_path, num_words, chars_dict):
-    title = "Frankenstein"
-    #Convert the dictionary into a list to simplify the sort
-    freq_list = [{"char": char, "num": count} for char, count in chars_dict.items()]
-    #Non-chronological order sorting
-    freq_list.sort(key =sort_on, reverse = True)
-
-
-    #Displaying my report
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the book '{title}'")
-    #Only displaying letters from the Alphabet
-    for item in freq_list:
-        if item['char'].isalpha():
-            print(f"The '{item['char']}' character was found {item['num']} times")
-    print(f"--- End report ---")
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,39 @@ def get_num_words(text):
     words = text.split()
     count = len(words)
     return count
+
+def get_chars_dict(text):
+    chars = {}
+
+    #checking each character in a lowercase string
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return  chars
+
+
+def sort_on(dict):
+    return dict["num"]
+
+def generate_report(book_path, num_words, chars_dict):
+    title = "Frankenstein"
+    #Convert the dictionary into a list to simplify the sort
+    freq_list = [{"char": char, "num": count} for char, count in chars_dict.items()]
+    #Non-chronological order sorting
+    freq_list.sort(key =sort_on, reverse = True)
+
+
+    #Displaying my report
+    print(f"============ BOOKBOT ============")
+    print(f"--- Analyzing book found at {book_path}...")
+    print(f"----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print(f"--------- Character Count -------")
+    #Only displaying letters from the Alphabet
+    for item in freq_list:
+        if item['char'].isalpha():
+            print(f"{item['char']}: {item['num']}")
+    print(f"============ END ============")
